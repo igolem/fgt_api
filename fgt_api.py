ef7a3049c9d911f2c67459180e1fe4856a6b0eef
Jason Mueller <15204665+igolem@users.noreply.github.com>
2019-03-07 20:59:44 -0600

    Update fgt_api.py

--- fgt_api.py
+++ fgt_api.py
@@ -3,7 +3,7 @@
 # file: fgt_api.py
 # author: jason mueller
 # created: 2018-12-26
-# last modified: 2019-02-24
+# last modified: 2019-03-07
 
 # purpose:
 # FortiGate API module for use with token-based authentication
